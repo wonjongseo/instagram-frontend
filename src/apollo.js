69a47455220ef8a602163ed42a1dd650be53db7b
wonjongseo <visionwill@naver.com>
2022-03-03 23:07:12 +0900

    add login mutation

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,21 @@
-import { makeVar } from "@apollo/client";
+import {ApolloClient, InMemoryCache, makeVar} from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
+const AUTHORIZATION = "authorization";
+
+export const isLoggedInVar = makeVar(localStorage.getItem(AUTHORIZATION));
 export const darkModeVar = makeVar(false);
+
+export const logUserIn = (token) => {
+    localStorage.setItem(AUTHORIZATION, token);
+    isLoggedInVar(true);
+};
+
+export const logUserOut = () => {
+    localStorage.removeItem(AUTHORIZATION);
+    isLoggedInVar(false);
+};
+
+export const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
+});
