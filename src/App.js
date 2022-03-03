66d4b834b06f38397f2194596c57155192ecfd63
wonjongseo <visionwill@naver.com>
2022-03-03 22:15:40 +0900

    login UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import {useReactiveVar} from "@apollo/client";
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-import {darkModeVar, isLoggedInVar} from "./apollo";
+import { useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import {ThemeProvider} from "styled-components";
-import {darkTheme, GlobalStyles, lightTheme} from "./styles";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+import SignUp from "./screens/SignUp";
+import routes from "./routes";
+
 function App() {
-    const isLoggedIn = useReactiveVar(isLoggedInVar);
-    const darkMode = useReactiveVar(darkModeVar);
-    return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-            <GlobalStyles />
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        {isLoggedIn ? <Home /> : <Login />}
-                    </Route>
-                    <Route>
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </Router>
-        </ThemeProvider>
-    );
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path={routes.home} exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          {!isLoggedIn ? (
+            <Route path={routes.signUp}>
+              <SignUp />
+            </Route>
+          ) : null}
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,4 +1,4 @@
 function NotFound() {
-    return <h1>NotFound</h1>;
+  return <h1>Not Found</h1>;
 }
 export default NotFound;
