3060a296b1e29ccc6904b46ee99eca48ecfec269
wonjongseo <visionwill@naver.com>
2022-03-03 23:59:54 +0900

    add header UI

--- src/apollo.js
+++ src/apollo.js
@@ -1,5 +1,10 @@
-import {ApolloClient, InMemoryCache, makeVar} from "@apollo/client";
-
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+    makeVar,
+} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
 const TOKEN = "token";
 const DARK_MODE = "DARK_MODE";
 
@@ -28,8 +33,20 @@ export const disableDarkMode = () => {
     localStorage.removeItem(DARK_MODE);
     darkModeVar(false);
 };
+const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, {headers}) => {
+    return {
+        headers: {
+            ...headers,
+            token: localStorage.getItem(TOKEN),
+        },
+    };
+});
 
 export const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
